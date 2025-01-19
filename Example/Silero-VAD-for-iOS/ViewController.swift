97319a2af9c5ef48823dc37155d20b8adc6553be
Your Name <fengzhou@gmail.com>
2025-01-19 21:15:51 +0800

    chore: Update Swift version to 4.2 and refactor audio session setup

--- Example/Silero-VAD-for-iOS/ViewController.swift
+++ Example/Silero-VAD-for-iOS/ViewController.swift
@@ -150,7 +150,7 @@ class ViewController: UIViewController {
             // Configure audio session
             let audioSession = AVAudioSession.sharedInstance()
             do {
-                try audioSession.setCategory(.playback, mode: .default, options: [])
+                try audioSession.setCategory(.playback, options: [])
                 try audioSession.setActive(true)
                 try audioSession.overrideOutputAudioPort(.speaker)
             } catch let error as NSError {
